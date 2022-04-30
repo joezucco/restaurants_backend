f5840a7dc72d625ad91e7b133c6b0a2e6a662689
Joe Zucco <josephzucco@gmail.com>
2022-04-30 12:19:07 -0400

    set up server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import restaurants from './api/restaurants.route.js';
+
+// require express
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json);
+
+app.use('/api/v1/restaurants', restaurants);
+app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+
+export default app
